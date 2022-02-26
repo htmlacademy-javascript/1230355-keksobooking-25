0091d96c57fd36b522b9c942bebfc263b8872cc5
Anna-Khvatova <khvatova.ann@yandex.ru>
2022-02-26 13:13:05 +0300

    Добавлена проверка параметров на положительные числа

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
 function getRandom(min, max) {
-  if (max < min) {
-    return undefined;
+  if (max >= min && min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  else if (max === min) {
-    return min;
-  }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return undefined;
 }
 
 function getRandomPre(min, max, precision) {
-  if (max < min) {
-    return undefined;
+  if (max >= min && min >= 0) {
+    return (Math.random() * (max - min) + min).toFixed(precision);
   }
-  else if (max === min) {
-    return min;
-  }
-  return (Math.random() * (max - min) + min).toFixed(precision);
+  return undefined;
 }
 
 getRandom(1,5);
