d3610a0ef23c7d01413f1208c6b7af5b3ad825ae
Anna-Khvatova <khvatova.ann@yandex.ru>
2022-03-14 22:49:44 +0300

    add rendering card by template

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-import {offerInfos} from './data.js';
-offerInfos();
+import { offerInfos } from './data.js';
+import { generationCard } from './genCard.js';
+
+const card = generationCard(offerInfos[0]);
+
+const mapContent = document.querySelector('#map-canvas');
+
+mapContent.appendChild(card);
