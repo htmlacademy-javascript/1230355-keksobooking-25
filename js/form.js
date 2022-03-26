61b013ea296a66fcaa4b5a075ec5c820da9ce8d9
Anna-Khvatova <khvatova.ann@yandex.ru>
2022-03-26 14:08:05 +0300

    Вывод карты с метками

--- js/form.js
+++ js/form.js
@@ -79,4 +79,35 @@ timeOutField.addEventListener('change', () => {
   timeInField.value = timeOutField.value;
 });
 
+// const sliderElement = document.querySelector('.ad-form__slider');
+// const valueElement = document.querySelector('#price');
+
+// noUiSlider.create(sliderElement, {
+//   range: {
+//     min: 0,
+//     max: 100000,
+//   },
+//   start: 0,
+//   step: 1,
+//   connect: 'lower',
+//   format: {
+//     to: function (value) {
+//       if (Number.isInteger(value)) {
+//         return value.toFixed(0);
+//       }
+//       return value.toFixed(1);
+//     },
+//     from: function (value) {
+//       return parseFloat(value);
+//     },
+//   },
+// });
+
+// sliderElement.noUiSlider.on('update', () => {
+//   valueElement.value = sliderElement.noUiSlider.get();
+// });
+// // valueElement.addEventListener('input', () => {
+// //   sliderElement.value = valueElement.noUiSlider.get();
+// // });
+
 export{deactiveForm, activeForm};
