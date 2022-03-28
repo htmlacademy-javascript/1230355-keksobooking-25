68fa31752113ac2697466071b85de796023c6a2c
Anna-Khvatova <khvatova.ann@yandex.ru>
2022-03-28 22:30:31 +0300

    changing the names of constants in the util module to capital letters

--- js/util.js
+++ js/util.js
@@ -13,21 +13,21 @@ const getRandomPre = (min, max, precision) => {
 };
 
 const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+  const LOWER = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const UPPER = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const RESULT = Math.random() * (UPPER - LOWER + 1) + LOWER;
+  return Math.floor(RESULT);
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const getRandomArrayFromDataSet = (dataSet) => {
-  const arr = Array.from(dataSet);
-  const result = new Array(getRandomPositiveInteger(0, arr.length - 1));
-  for (let i = 0; i < result.length; i++) {
-    result[i] = arr.splice(getRandomPositiveInteger(0, arr.length - 1), 1);
+  const ARR = Array.from(dataSet);
+  const RESULT = new Array(getRandomPositiveInteger(0, ARR.length - 1));
+  for (let i = 0; i < RESULT.length; i++) {
+    RESULT[i] = ARR.splice(getRandomPositiveInteger(0, ARR.length - 1), 1);
   }
-  return result;
+  return RESULT;
 };
 
-export {getRandom, getRandomPre, getRandomArrayElement, getRandomArrayFromDataSet};
+export { getRandom, getRandomPre, getRandomArrayElement, getRandomArrayFromDataSet };
